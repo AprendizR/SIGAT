c237c8e67d68fcb88c5e233c72320febfee41824
Renan Rosa <renanrosa1005@gmail.com>
2026-05-31 19:43:02 -0300

    Configura Vite para múltiplas páginas

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/SIGAT/'
+  base: '/SIGAT/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        novo: resolve(__dirname, 'novo.html'),
+        menu: resolve(__dirname, 'menu.html')
+      }
+    }
+  }
 })
